57f8d1c6f281855fb22f3b697159b4f6ffa28651
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-17 13:45:24 +0200

    Player component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
+import Player from './components/Player.jsx';
+
 function App() {
   return (
-    <div id="game-container">
-      <ol id="players">
-        <li>
-          <span className="player-name">Player 1</span>
-          <span className="player-symbol">X</span>
-        </li>
-        <li>
-          <span className="player-name">Player 2</span>
-          <span className="player-symbol">0</span>
-        </li>
-      </ol>
-      Game Board
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
   );
 }
 
