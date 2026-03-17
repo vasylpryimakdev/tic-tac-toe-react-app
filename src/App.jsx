958db21eef21c67730cd5434b22ac1d31f4c3cb5
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-17 13:30:50 +0200

    App component basic markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
 function App() {
-  
-
   return (
-    <p>Comming soon...</p>
-  )
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>
+          <span className="player-symbol">0</span>
+        </li>
+      </ol>
+      Game Board
+    </div>
+  );
 }
 
-export default App
+export default App;
