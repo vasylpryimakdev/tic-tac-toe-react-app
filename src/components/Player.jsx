f16b0846dea5c7461cdb84985c25119f25e717fd
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-17 15:51:53 +0200

    add player name editing, game over logic, and rematch functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -13,13 +13,11 @@ export default function Player({ initialName, symbol, isActive }) {
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
-  // let btnCaption = 'Edit';
 
   if (isEditing) {
     editablePlayerName = (
       <input type="text" required value={playerName} onChange={handleChange} />
     );
-    // btnCaption = 'Save';
   }
 
   return (
