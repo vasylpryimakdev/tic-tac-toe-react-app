0cea70e4dc37530fca7fbdd312a94641525caee7
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-17 14:10:21 +0200

    Add player turn switching and active player highlight

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,15 +6,17 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
       const updatedBoard = structuredClone(prevGameBoard);
-      updatedBoard[rowIndex][colIndex] = "X";
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+
+    onSelectSquare();
   }
 
   return (
